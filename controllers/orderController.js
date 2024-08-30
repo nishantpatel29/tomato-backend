d461791a36b46c25222c4a0aae63767c4f48035b
Nishant Patel <nishantpatel197322@gmail.com>
2024-08-30 12:09:29 +0530

    change

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,7 +2,7 @@ import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
 import Stripe from "stripe";
 const stripe=new Stripe(process.env.STRIPE_SECRET_KEY)
-const url=process.env.URL
+const url=process.env.URL_
  export const placeOrder=async(req,res)=>{
 try {
     const{userId,items,amount,address}=req.body
